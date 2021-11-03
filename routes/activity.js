7c2da22cf4c1fdfbfdc708d82aad207ba5d3007d
blendmedia <heraldo@blendmedia.co.id>
2021-11-03 14:03:20 +0700

    #20 Attempt: reconfigure execute function

--- routes/activity.js
+++ routes/activity.js
@@ -106,7 +106,7 @@ exports.execute = function (req, res) {
     request({
         uri: url, 
         method: 'POST',
-        json: payload['arguments'].execute.inArguments
+        json: req.body
     }, function(error, response, body){
         if(!error){
             console.log(body);
